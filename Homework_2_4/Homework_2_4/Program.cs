efb76b4d2069ffabf67cb3c4f1771d5099666610
IvanTomash <tomashuckvanya@gmail.com>
2023-07-19 14:58:32 +0300

    First commit HM_2_4

---
+++ Homework_2_4/Homework_2_4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Homework_2_4
+{
+    /// <summary>
+    /// Main class.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Enrty point.
+        /// </summary>
+        /// <param name="args">Message.</param>
+        public static void Main(string[] args)
+        {
+            Starter starter = new Starter();
+            starter.Run();
+        }
+    }
+}
---
+++ Homework_2_4/Homework_2_4/Tomato.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_2_4
+{
+    /// <summary>
+    /// Big red fetus.
+    /// </summary>
+    internal class Tomato : Vegetable
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Tomato"/> class.
+        /// </summary>
+        /// <param name="weight">Represent amount of  vegetable.</param>
+        public Tomato(double weight)
+            : base("Tomato", 190, weight)
+        {
+        }
+    }
+}
